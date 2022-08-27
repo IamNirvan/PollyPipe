c7a2189df53eb5d66e793a7605c0823343aeeb1a
Shalin Kulawardane <nirvan.work@gmail.com>
2022-08-27 15:56:27 +0530

    Added utility programs

---
+++ PollyPipe/Utility/QueryHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PollyPipe
+{
+    class QueryHandler
+    {
+        private readonly string ConnectionString;
+
+        public QueryHandler(string connectionString = @"Data Source=DESKTOP-0CECDCR;Initial Catalog=PollyPipeV1;Integrated Security=True")
+        {
+            ConnectionString = connectionString;
+        }
+
+        public bool HandleInsertDeleteUpdateQuery(string query, string[] parameters, object[] values)
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                    if(parameters.Length != values.Length)
+                    {
+                        throw new Exception("Parameter array and values array length mismatch");
+                    }
+
+                    for (int i = 0; i < parameters.Length; i++)
+                    {
+                        sqlCommand.Parameters.AddWithValue(parameters[i], values[i]);
+                    }
+                    sqlCommand.ExecuteNonQuery();
+                }
+                return true;
+            } catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public DataTable SelectQueryHandler(string query)
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                    DataTable result = new DataTable();
+                    sqlDataAdapter.Fill(result);
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An exception occurred when selecting data:\n{e}");
+                return null;
+            }
+        }
+    }
+}
